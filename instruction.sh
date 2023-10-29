ac416c39e808b44e3ad9ac5038628d0c459b50bf
Matt <Matt>
2023-10-29 11:32:44 +0000

    Add and modify files

--- instruction.sh
+++ instruction.sh
@@ -1,6 +1,10 @@
 # Install mkdocs-material
 pip install mkdocs-material -q
 
+# Create a new mkdocs
+mkdocs new <NAME>
+mkdocs serve -a 0.0.0.0:8000
+
 # Install nginx
 sudo apt update
 sudo apt install nginx
