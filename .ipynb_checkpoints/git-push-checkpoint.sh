1dfa856046d8b71756edad6b7ad43270a6efa621
Matt <Matt>
2023-10-29 11:18:06 +0000

    Add and modify files

---
+++ .ipynb_checkpoints/git-push-checkpoint.sh
@@ -0,0 +1,11 @@
+# sh git-push.sh
+# sh g*
+
+git add .
+git config --global user.email "Matt"
+git config --global user.name "Matt"
+git commit -m "Add and modify files"
+git push
+
+# Username
+# Password/Token
