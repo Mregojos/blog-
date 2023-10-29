7cef7d5f25697f824164092d2bf8e9e79b8743d3
Matt <Matt>
2023-10-29 11:37:47 +0000

    Add and modify files

--- instruction.sh
+++ instruction.sh
@@ -3,7 +3,15 @@ pip install mkdocs-material -q
 
 # Create a new mkdocs
 mkdocs new <NAME>
+cd <NAME>
+# Build
+# In GCP create a new firewall with 0.0.0.0/0 and TCP:8000
+# Serve
 mkdocs serve -a 0.0.0.0:8000
+# Build
+mkdocs build
+# Build with specify LOCATION
+mkdocs build -a <LOCATION>
 
 # Install nginx
 sudo apt update
